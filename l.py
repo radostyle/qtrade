fb9d0ecca31de9a3b5bf7ec2ee39f1a115136b73
Jonathan Alvarado <jonathan.alvarado@sourceability.com>
2024-01-07 10:41:16 -0600

    Adding strategy for my SPY trading strategy

--- l.py
+++ l.py
@@ -37,42 +37,93 @@ class Strategy():
         data['Benchmark'] = data['Close'] / data['Close'].iloc[0] * 100.
         return data
 
-security_name = 'SSO'
-start_date = '2015-01-01'
-end_date = '2023-01-01'
+def plot_psar():
+    security_name = 'SSO'
+    start_date = '2015-01-01'
+    end_date = '2023-01-01'
 
-mystrategy = Strategy(security_name, start_date, end_date)
-print(mystrategy.security_name, mystrategy.start, mystrategy.end)
+    mystrategy = Strategy(security_name, start_date, end_date)
+    print(mystrategy.security_name, mystrategy.start, mystrategy.end)
 
-result = mystrategy.download_data()
-mypsar = mystrategy.get_psar(result)
+    result = mystrategy.download_data()
+    mypsar = mystrategy.get_psar(result)
 
-print(result)
-print("\n\n", mypsar)
+    print(result)
+    print("\n\n", mypsar)
 
 
-plt.plot(mypsar.index, mypsar.values, label='PSAR',linestyle='--' )
-plt.plot(result.index, result.Close, label='Close')
+    plt.plot(mypsar.index, mypsar.values, label='PSAR',linestyle='--' )
+    plt.plot(result.index, result.Close, label='Close')
 # Find crossover points
-cross_above = (mypsar > result.Close) & (mypsar.shift(1) <= result.Close.shift(1))
-cross_below = (mypsar < result.Close) & (mypsar.shift(1) >= result.Close.shift(1))
+    cross_above = (mypsar > result.Close) & (mypsar.shift(1) <= result.Close.shift(1))
+    cross_below = (mypsar < result.Close) & (mypsar.shift(1) >= result.Close.shift(1))
 # Plot green arrows for upward crossovers
-plt.scatter(result.index[cross_above], result.Close[cross_above], marker='^', color='green', label='Buy Signal')
-plt.scatter(result.index[cross_below], result.Close[cross_below], marker='v', color='red', label='Sell Signal')
-plt.grid(True)
-plt.legend()
-plt.show()
+    plt.scatter(result.index[cross_above], result.Close[cross_above], marker='^', color='green', label='Buy Signal')
+    plt.scatter(result.index[cross_below], result.Close[cross_below], marker='v', color='red', label='Sell Signal')
+    plt.grid(True)
+    plt.legend()
+    plt.show()
 
 
 
-signal = mystrategy.get_signals(result,  mypsar)
-result["Signal"] = signal
+    signal = mystrategy.get_signals(result,  mypsar)
+    result["Signal"] = signal
 
-result = mystrategy.get_equity_curve(result)
-plt.plot(result['Cumulative Returns'])
+    result = mystrategy.get_equity_curve(result)
+    plt.plot(result['Cumulative Returns'])
 # plt.plot(result['Signal'])
-plt.show()
+    plt.show()
 
+class OtherStrategy():
+    def __init__(self, security_name, start, end):
+        self.security_name = security_name
+        self.start = start
+        self.end = end
+
+    def download_data(self):
+        security = yf.Ticker(self.security_name)
+        data = security.history(interval='1d', start=self.start, end=self.end)
+        return data
+    def calculate_trading_days(self, result):
+        result["constant"] =1
+        result["trading_day_of_year"]=result.groupby(pd.Grouper(freq='Y'))[['constant']].cumsum()
+    def my_strategy_signal(self, result):
+        self.pick_dates = ((result["trading_day_of_year"] >= 50) & (result["trading_day_of_year"] <= 84)) | ((result["trading_day_of_year"] >= 93) & (result["trading_day_of_year"] <= 153)) | ((result["trading_day_of_year"] >= 188) & (result["trading_day_of_year"] <= 251))
+
+        signal = np.where(self.pick_dates== True, 1, 0)
+        return signal
+
+    def run(self):
+        result = self.download_data()
+        self.calculate_trading_days(result)
+        equity_curve = self.calculate(result)
+
+        plt.plot(equity_curve['Cumulative Returns'])
+    def calculate(self, result):
+        my_strategy_signal = self.my_strategy_signal(result)
+        result['Signal'] = my_strategy_signal
+        equity_curve = self.get_equity_curve2(result)
+        return equity_curve
+    def get_psar(self, data):
+        myindicator = ta.trend.PSARIndicator(data.High, data.Low, data.Close)
+        psar_series = myindicator.psar()
+        return psar_series
+    def get_signals(self, data, indicator):
+        signal = np.where(data.Close > indicator, 1, np.where(data.Close < indicator, -1, 0))
+        return signal
+    def get_equity_curve(self, data):
+        data['Daily Returns'] = data['Close'].pct_change() * data['Signal'].shift(1)
+        # data['Cumulative Returns'] = (1.0 * data['Daily Returns']).cumprod() * 100.0
+        data['Cumulative Returns'] = (1.0 + data['Daily Returns']).cumprod()
+        data['Benchmark'] = data['Close'] / data['Close'].iloc[0] * 100.
+        return data
+
+    def get_equity_curve2(self, data):
+        data['Daily Returns'] = data['Close'].pct_change() * data['Signal']
+        # data['Cumulative Returns'] = (1.0 * data['Daily Returns']).cumprod() * 100.0
+        data['Cumulative Returns'] = (1.0 + data['Daily Returns']).cumprod()
+        data['Benchmark'] = data['Close'] / data['Close'].iloc[0] * 100.
+        return data
 #https://medium.com/analytics-vidhya/a-simple-day-and-night-strategy-using-python-a36c18578161
 # need to know days that are 5 business days before end
 # 01=1,02=2,04=3
